7d6c95cb7eac8d14709f868c5632f87a82e1de6e
AndreaCimetta <andrea.91cime@gmail.com>
2021-04-07 17:12:35 +0200

    update from angular 9 to angular 11

--- frontend/src/app/auth/password/request-reset/request-reset.component.spec.ts
+++ frontend/src/app/auth/password/request-reset/request-reset.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RequestResetComponent } from './request-reset.component';
 
@@ -6,7 +6,7 @@ describe('RequestResetComponent', () => {
   let component: RequestResetComponent;
   let fixture: ComponentFixture<RequestResetComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RequestResetComponent ]
     })
